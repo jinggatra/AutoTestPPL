e2dbc055435bad32ada1c6c4061944657d6858ab
jinggatra <wmkbisa@gmail.com>
2019-11-06 11:19:02 +0700

    bingung

--- testing.js
+++ testing.js
@@ -4,33 +4,37 @@
 // |                                   |
 // -------------------------------------
 
-var webdriver = require('selenium-webdriver')
-const chrome = require('selenium-webdriver/chrome')
-const chromedriver = require('chromedriver')
-SeleniumServer = require("selenium-webdriver/remote").SeleniumServer;
+// var webdriver = require('selenium-webdriver')
+// const chrome = require('selenium-webdriver/chrome')
+// const chromedriver = require('chromedriver')
+// SeleniumServer = require("selenium-webdriver/remote").SeleniumServer;
+const {Builder, By, Key, Util} = require ("selenium-webdriver");
 
-chrome.setDefaultService(
-    new chrome.ServiceBuilder(chromedriver.path).build())
-
-var driver = new webdriver.Builder().forBrowser('chrome').build()
+// chrome.setDefaultService(
+//     new chrome.ServiceBuilder(chromedriver.path).build())
 
 //------------------------------------------------------------------------
 //Testing
+async function testing(){
+    let driver = await new Builder().forBrowser("chrome").build();
 
-
-async () =>{
-    await driver.get('http://www.youtube.com')
-
-    const searchInput = await driver.findElement(webdriver.By.name('search_query'))
-
-    await searchInput.sendKeys('Dr Phil')
-
-    const button = await driver.findElement(webdriver.By.id('search-icon-legacy'))
-    const searchButton = button[1]
-
-    await searchButton.click()
-
-    await driver.sleep(2000)
-
-    driver.quit()
+    await driver.get("http://www.youtube.com");
+    await driver.findElement(By.name("search_query")).sendKeys("Dr Phil", Key.RETURN);
 }
+testing();
+
+// async () =>{
+//     await driver.get('http://www.youtube.com')
+//     const searchInput = await driver.findElement(webdriver.By.name('search_query'))
+
+//     await searchInput.sendKeys('Dr Phil')
+
+//     const button = await driver.findElement(webdriver.By.id('search-icon-legacy'))
+//     const searchButton = button[1]
+
+//     await searchButton.click()
+
+//     await driver.sleep(2000)
+
+//     driver.quit()
+// }
